a8e58069eb3c7be5b15cbe1ab515d1999b4a7e17
Corey Recai <crecai@ITCS.LOCAL>
2020-07-10 19:00:56 -0400

    Bulding out submittal upload

---
+++ projects/B18-411/calendar.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,listYear',
+        },
+
+        themeSystem: 'standard',
+        displayEventTime: true,
+        eventColor: '#F09300',
+
+
+        // API KEY
+        googleCalendarApiKey: 'AIzaSyAlERdHK-QuZ16KCTgKAwL368mGVLoEzrs',
+
+        // Academy of American Studies Calendar
+        events: {
+            googleCalendarId: '6omfgn5m3vscfab3qs9pgj7308@group.calendar.google.com',
+        },
+
+        eventClick: function (arg) {
+            // opens events in a popup window
+            window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');
+
+            arg.jsEvent.preventDefault() // don't navigate in main tab
+        },
+
+        loading: function (bool) {
+            document.getElementById('loading').style.display =
+                bool ? 'block' : 'none';
+        }
+
+    });
+
+    calendar.render();
+});
